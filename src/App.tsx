b3fb6d4e22c2cf61900f52b7ae3faa0a92fe6e2b
eyusd <eyusd.gaming@gmail.com>
2023-07-22 14:39:50 +0200

    block part.III

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-
-import { Box } from "@mui/material";
-import { Block } from "./components/Block/block";
-import { BlockType, Conditions, Functions } from "./shared/functions";
+import { SideBar } from "./components/SideBar";
+import { CodeEditor } from "./components/CodeEditor";
 
 function App() {
-	return (
-		<Box sx={{display: "flex", gap: "10px", alignItems: "center"}}>
-			<Block type={BlockType.FUNCTION} content={{ name: Functions.ADD, context: undefined }} topBar={{ text: "Case: 1" }} dot={"both"}/>
-			<Block type={BlockType.CONDITION} content={{ name: Conditions.SWITCH, context: undefined }} bottomBar={{ text: "Switch: var" }} />
-			<Block type={BlockType.FUNCTION} dot={"both"}/>
-		</Box>
-	);
+  return (
+    <>
+      <CodeEditor />
+      <SideBar />
+    </>
+  );
 }
 
 export default App;
