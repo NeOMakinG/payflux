37fd805dbf14a3d53a537e6a2313e5a7eec6596d
0xm4king <0xm4king@0xm4kings-MacBook-Air.local>
2023-07-22 14:46:41 +0200

    feat: add CodeEditor component

---
+++ src/components/CodeEditor/index.tsx
@@ -0,0 +1,58 @@
+import { Box, useTheme } from "@mui/material";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import vscDarkPlus from "react-syntax-highlighter/dist/esm/styles/prism/vsc-dark-plus";
+
+export type Snippet = {
+  value: string;
+};
+
+type CodeEditorProps = {
+  snippets: Array<Snippet>;
+  highlightedIndex?: number;
+};
+
+export const CodeEditor: React.FC<CodeEditorProps> = ({
+  snippets,
+  highlightedIndex,
+}) => {
+  const theme = useTheme();
+
+  const highlightedStyle = {
+    border: "2px solid #9f9000",
+    zoom: "1.1",
+    margin: "5px 0",
+    padding: "2px",
+    borderRadius: "10px",
+    zIndex: 2,
+  };
+
+  return (
+    <Box>
+      <Box
+        borderRadius={theme.custom.padding.large}
+        bgcolor={theme.palette.background.dark}
+        border={`1px solid ${theme.palette.border.light}`}
+        width="100%"
+        p={4}
+      >
+        {snippets.map((snippet, index) => (
+          <SyntaxHighlighter
+            customStyle={{
+              margin: 0,
+              padding: 0,
+              position: "relative",
+              border: "1px solid transparent",
+              transition: ".25s ease-out",
+              backgroundColor: theme.palette.background.dark,
+              ...(index === highlightedIndex ? highlightedStyle : null),
+            }}
+            style={vscDarkPlus}
+            language="solidity"
+          >
+            {snippet.value}
+          </SyntaxHighlighter>
+        ))}
+      </Box>
+    </Box>
+  );
+};
--- src/theme.ts
+++ src/theme.ts
@@ -1,71 +1,84 @@
 import { PaletteOptions, createTheme } from "@mui/material";
 
 type ColorsObject = {
-	background: {
-		default: string;
-		dark: string;
-		sidebar: string;
-	};
-	gradient: {
-		red: string;
-	};
+  background: {
+    default: string;
+    dark: string;
+    sidebar: string;
+  };
+  border: {
+    light: string;
+  };
+  gradient: {
+    red: string;
+  };
 };
 
 type CustomVariables = {
-	borderRadius: {
-		default: string;
+  borderRadius: {
+    default: string;
     small: string;
-	};
+  };
+  padding: {
+    default: string;
+    large: string;
+  };
 };
 
 declare module "@mui/material/styles" {
-
   interface TypeBackground {
-		default: string;
-		dark: string;
+    default: string;
+    dark: string;
     paper: string;
-		sidebar: string;
-	}
+    sidebar: string;
+  }
 
   interface Theme {
     custom: CustomVariables;
   }
 
   interface ThemeOptions {
-		custom: CustomVariables;
-	}
+    custom: CustomVariables;
+  }
 
-	// eslint-disable-next-line @typescript-eslint/no-empty-interface
-	interface Palette extends ColorsObject {}
-	// eslint-disable-next-line @typescript-eslint/no-empty-interface
-	interface PaletteOptions extends ColorsObject {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  interface Palette extends ColorsObject {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  interface PaletteOptions extends ColorsObject {}
 }
 
 const palette: PaletteOptions = {
-	background: {
-		default: "#282828",
-		dark: "#212121",
-		sidebar: "#2D2D2D",
-	},
+  background: {
+    default: "#282828",
+    dark: "#212121",
+    sidebar: "#2D2D2D",
+  },
+  border: {
+    light: "#4A4A4A",
+  },
   text: {
     primary: "#ffffff",
   },
-	gradient: {
-		red: "linear-gradient(30deg, #f20486, #ff6068)",
-	},
+  gradient: {
+    red: "linear-gradient(30deg, #f20486, #ff6068)",
+  },
 };
 
 const custom: CustomVariables = {
   borderRadius: {
     default: "23px",
-    small: "8px"
-  }
+    small: "8px",
+  },
+  padding: {
+    default: "12px",
+    large: "30px",
+  },
 };
 
 export const theme = createTheme({
-	palette,
-	custom,
+  palette,
+  custom,
   typography: {
-    fontFamily: "Inconsolata"
+    fontFamily: "Inconsolata",
   },
 });
