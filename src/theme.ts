0f07f9105ab166ab51826dbde19c8b2bd25fe73e
Mathieu <mathieu.raimbault@hotmail.com>
2023-07-22 12:37:24 +0200

    :lipstick: add theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,3 +1,65 @@
-import { createTheme } from "@mui/material";
+import { PaletteOptions, createTheme } from "@mui/material";
 
-export const theme = createTheme();
+type ColorsObject = {
+	background: {
+		default: string;
+		dark: string;
+		sidebar: string;
+	};
+	gradient: {
+		red: string;
+	};
+};
+
+type CustomVariables = {
+	borderRadius: {
+		default: string;
+    small: string;
+	};
+};
+
+declare module "@mui/material/styles" {
+
+  interface TypeBackground {
+		default: string;
+		dark: string;
+    paper: string;
+		sidebar: string;
+	}
+
+
+  interface ThemeOptions {
+		custom: CustomVariables;
+	}
+
+	// eslint-disable-next-line @typescript-eslint/no-empty-interface
+	interface Palette extends ColorsObject {}
+	// eslint-disable-next-line @typescript-eslint/no-empty-interface
+	interface PaletteOptions extends ColorsObject {}
+}
+
+const palette: PaletteOptions = {
+	background: {
+		default: "#282828",
+		dark: "#212121",
+		sidebar: "#2D2D2D",
+	},
+	gradient: {
+		red: "linear-gradient(30deg, #f20486, #ff6068)",
+	},
+};
+
+const custom: CustomVariables = {
+  borderRadius: {
+    default: "23px",
+    small: "8px"
+  }
+};
+
+export const theme = createTheme({
+	palette,
+	custom,
+	typography: {
+		fontFamily: "Inconsolata",
+	},
+});
