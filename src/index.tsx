f40e8e37efb62cf475141aa7f706474a9be477ea
eyusd <eyusd.gaming@gmail.com>
2023-07-22 23:01:15 +0200

    subcomponents part.III

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,20 @@ import App from "./App";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
 import reportWebVitals from "./reportWebVitals";
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
 import "@fontsource/inconsolata"; // Defaults to weight 400
 import "@fontsource/inconsolata/400.css"; // Specify weight
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </LocalizationProvider>
   </React.StrictMode>
 );
 
