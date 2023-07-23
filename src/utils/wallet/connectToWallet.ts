6d4b64370934ce20eca8306b2c81140ef8add033
0xm4king <github@0xm4king.com>
2023-07-23 05:52:31 +0200

    fix: show on success

--- src/utils/wallet/connectToWallet.ts
+++ src/utils/wallet/connectToWallet.ts
@@ -1,24 +1,27 @@
 import { getWalletProvider } from "./getWalletProvider";
 
 export const connectToWallet = async () => {
-	const provider = await getWalletProvider();
-	const accountFrom = await provider.request({ method: "eth_accounts" });
+  const provider = await getWalletProvider();
+  const accountFrom = await provider.request({ method: "eth_accounts" });
 
-	return {
-		accountFrom,
-		provider,
-	};
+  return {
+    accountFrom,
+    provider,
+  };
 };
 
 export const deployContract = async (bytecode: string) => {
-	const { accountFrom, provider } = await connectToWallet();
-	await provider.request({
-		method: "eth_sendTransaction",
-		params: [
-			{
-				from: accountFrom,
-				data: bytecode,
-			},
-		],
-	});
-}
+  const { accountFrom, provider } = await connectToWallet();
+  const response = await provider.request({
+    method: "eth_sendTransaction",
+    params: [
+      {
+        from: accountFrom,
+        to: "0x0000000000000000000000000000000000000000",
+        data: bytecode,
+      },
+    ],
+  });
+
+  return response;
+};
